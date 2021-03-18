1053d1b272c42648bfd5a2e759a23e0860397cc4
Anu <Anu.Shrestha@syngenta.com>
2021-03-17 19:04:21 -0500

    heroku

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 
 const { Telegraf } = require('telegraf')
 require('dotenv').config();
+const axios = require("axios");
+const FACT_URL = 'https://uselessfacts.jsph.pl/random.json?language=en'
+const INSULT_URL = 'https://insult.mattbas.org/api/insult.json'
+const token = process.env.TOKEN;
 
 
-const bot = new Telegraf(process.env.TOKEN)
+let bot;
+ 
+if (process.env.NODE_ENV === 'production') {
+   bot = new TelegramBot(token);
+   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+   bot = new TelegramBot(token, { polling: true });
+}
 
-bot.on('text', (ctx) => {
+
+bot.on('text', async (ctx) => {
   let name = ctx.update.message.from.first_name 
   const isBot = ctx.update.message.from.is_bot
   let string = ctx.update.message.text.toLowerCase()
-  console.log(name)
   if (name  === 'Brock') {
     if (string.includes(' bet ') || string.includes('betting')) {
       ctx.reply("'Nahhhhh' -Lucas Brandl")
@@ -23,6 +34,20 @@ bot.on('text', (ctx) => {
   }
   if (string.includes('sko buffs') && !isBot) {
     ctx.reply('Sko Buffs')
+  }
+  if (string.includes('csu') && !isBot) {
+    ctx.reply('I said it sucks to be a CSU Ram!')
+  }
+  if (string.includes('fun fact')) {
+    try {
+      const response = await axios.get(FACT_URL)
+      console.log(response)
+      if (response.data && response.data.text) { 
+        ctx.reply(response.data.text)
+      }
+    } catch(e){
+      ctx.reply("Not today I'm broken")
+    }
   }
 })
 
