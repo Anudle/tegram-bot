0a6ca0acca7f3fb99129fa7527c3df045da38303
Kyle Bales <masterjedi@gmail.com>
2023-01-19 16:56:48 -0800

    update quote bot url

--- api.js
+++ api.js
@@ -21,9 +21,9 @@ const getRandomPhoto = async () => {
 const kylesAPI = async (text, author) => {
   let quotezURL
   if (author) {
-    quotezURL = `http://dumb.kylebales.com/quotez?auth=NiVZfG1yvThe9nhR0YxurQ&quote=${encodeURIComponent(text)}&author=${author}`
+    quotezURL = `http://quote.heliumlink.com/quotez?auth=NiVZfG1yvThe9nhR0YxurQ&quote=${encodeURIComponent(text)}&author=${author}`
   } else {
-    quotezURL = `http://dumb.kylebales.com/quotez?auth=NiVZfG1yvThe9nhR0YxurQ&quote=${encodeURIComponent(text)}`
+    quotezURL = `http://quote.heliumlink.com/quotez?auth=NiVZfG1yvThe9nhR0YxurQ&quote=${encodeURIComponent(text)}`
   }
   console.log({quotezURL})
   let response
